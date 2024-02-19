23a8ec2d6eea8aaee0ef495aa4671bc7c613bf63
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-02-19 17:43:59 +0000

    Added basic queries for exercise and muscle_group tables/repos

--- lib/database/tables/exercise/exercise_repository.dart
+++ lib/database/tables/exercise/exercise_repository.dart
@@ -1,13 +1,17 @@
 import 'package:gym_bro/database/database_connector.dart';
 import 'package:gym_bro/database/tables/exercise/exercise_constants.dart';
 
+import 'exercise_table_object.dart';
+
 class ExerciseRepository {
   final DatabaseHelper databaseHelper;
 
   ExerciseRepository(this.databaseHelper);
 
-  Future<void> getAllExercises () async {
+  Future<List<Exercise>> getAllExercises () async {
     final db = await databaseHelper.database;
     final List<Map<String, dynamic>> allExercises = await db.query(tableName);
+
+    return allExercises.map((exercise) => Exercise.fromMap(exercise)).toList();
   }
 }
--- lib/database/tables/muscle_group/muscle_group_repository.dart
+++ lib/database/tables/muscle_group/muscle_group_repository.dart
@@ -1,7 +1,20 @@
+import 'package:gym_bro/database/tables/muscle_group/muscle_group_object.dart';
+import 'package:gym_bro/database/tables/muscle_group/muscle_group_constants.dart';
+
 import '../../database_connector.dart';
 
 class MuscleGroupRepository {
   final DatabaseHelper databaseHelper;
 
   MuscleGroupRepository(this.databaseHelper);
+
+  Future<List<MuscleGroup>> getAllMuscleGroups() async {
+    final db = await databaseHelper.database;
+    final List<Map<String, dynamic>> allMuscleGroups =
+        await db.query(tableName);
+
+    return allMuscleGroups
+        .map((muscleGroup) => MuscleGroup.fromMap(muscleGroup))
+        .toList();
+  }
 }
