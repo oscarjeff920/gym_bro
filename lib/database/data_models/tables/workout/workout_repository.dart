862f2f49d1faa4f2df86e895b19999e0b1377f4a
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-02-22 17:20:55 +0000

    added getAllWorkouts method to retrieve all workouts in the database

--- lib/database/data_models/tables/workout/workout_repository.dart
+++ lib/database/data_models/tables/workout/workout_repository.dart
@@ -1,7 +1,16 @@
+import 'package:gym_bro/database/data_models/tables/table_constants.dart';
+import 'package:gym_bro/database/data_models/tables/workout/workout_object.dart';
 import 'package:gym_bro/database/database_connector.dart';
 
 class WorkoutRepository {
   final DatabaseHelper databaseHelper;
 
   WorkoutRepository(this.databaseHelper);
+
+  Future<List<Workout>> getAllWorkouts() async {
+    final db = await databaseHelper.database;
+
+    final List<Map<String, dynamic>> workouts = await db.query(workoutTableName);
+    return workouts.map((workout) => Workout.fromMap(workout)).toList();
+  }
 }
