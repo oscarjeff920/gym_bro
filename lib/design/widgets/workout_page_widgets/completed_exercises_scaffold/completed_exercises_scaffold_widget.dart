7c6eff80cbaf5490e0647fc19a3d085cfdbdf632
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-03-12 11:56:26 +0000

    added finish workout button which is shown on new workouts but disabled unless at least 1 exercise has been saved to the state

--- lib/design/widgets/workout_page_widgets/completed_exercises_scaffold/completed_exercises_scaffold_widget.dart
+++ lib/design/widgets/workout_page_widgets/completed_exercises_scaffold/completed_exercises_scaffold_widget.dart
@@ -2,21 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gym_bro/constants/enums.dart';
 import 'package:gym_bro/data_models/FE_data_models/exercise_data_models.dart';
-import 'package:gym_bro/data_models/FE_data_models/workout_data_models.dart';
-import 'package:gym_bro/state_management/blocs/database_tables/exercise/exercise_table_operations_bloc.dart';
-import 'package:gym_bro/state_management/blocs/database_tables/exercise/exercise_table_operations_state.dart';
-import 'package:gym_bro/state_management/blocs/database_tables/workout/workout_table_operations_bloc.dart';
-import 'package:gym_bro/state_management/blocs/database_tables/workout/workout_table_operations_event.dart';
-import 'package:gym_bro/state_management/cubits/active_workout_cubit/active_workout_cubit.dart';
 import 'package:gym_bro/state_management/cubits/active_workout_cubit/active_workout_cubit.dart';
 import 'package:gym_bro/state_management/cubits/active_workout_cubit/active_workout_state.dart';
-import 'package:gym_bro/state_management/cubits/data_model_cubits/workout_page/workout_page_workout_cubit/workout_page_workout_cubit.dart';
-import 'package:gym_bro/state_management/cubits/data_model_cubits/workout_page/workout_page_workout_cubit/workout_page_workout_state.dart';
-import 'package:gym_bro/state_management/cubits/workout_timer_cubit/workout_timer_cubit.dart';
-import 'package:gym_bro/state_management/cubits/workout_timer_cubit/workout_timer_state.dart';
 
 import 'exercise_tiles/add_new_exercise_tile_widget.dart';
 import 'exercise_tiles/completed_exercise_tile_widget.dart';
+import 'finish_workout_button_widget.dart';
 
 class CompletedExercisesScaffold extends StatelessWidget {
   final double tileSpacingValue;
@@ -68,44 +59,23 @@ class CompletedExercisesScaffold extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.only(bottom: tileSpacingValue),
               child: Ink(
-                decoration: BoxDecoration(shape: BoxShape.rectangle),
+                decoration: const BoxDecoration(shape: BoxShape.rectangle),
                 width: 50,
                 height: 50,
                 child: AspectRatio(
                   aspectRatio: 1,
                   child: BlocBuilder<ActiveWorkoutCubit, ActiveWorkoutState>(
                     builder: (activeWorkoutContext, activeWorkoutState) {
-                      return BlocBuilder<WorkoutTimerCubit, WorkoutTimerState>(
-                        builder: (timerContext, timerState) {
-                          return TextButton(
-                            child: const Icon(
-                              Icons.check_box,
-                              size: 50,
-                            ),
-                            onPressed: () {
-                              if (exercises.isNotEmpty &&
-                                  activeWorkoutState is NewActiveWorkoutState) {
-                                BlocProvider.of<WorkoutTimerCubit>(context)
-                                    .stopTimer();
-                                BlocProvider.of<WorkoutTableOperationsBloc>(
-                                        context)
-                                    .add(InsertNewWorkoutIntoTableEvent(
-                                        newWorkout: NewWorkoutModel(
-                                            day: activeWorkoutState.day,
-                                            month: activeWorkoutState.month,
-                                            year: activeWorkoutState.year,
-                                            workoutDuration:
-                                                timerState.toString(),
-                                            exercises: activeWorkoutState.exercises)));
-                                BlocProvider.of<WorkoutTimerCubit>(context)
-                                    .resetTimer();
-                                BlocProvider.of<WorkoutTableOperationsBloc>(context).add(QueryAllWorkoutTableEvent());
-                                Navigator.of(context).pushNamed("/");
-                              }
-                            },
-                          );
-                        },
-                      );
+                      switch (activeWorkoutState) {
+                        case NewActiveWorkoutState():
+                          return FinishWorkoutButton(
+                              day: activeWorkoutState.day,
+                              month: activeWorkoutState.month,
+                              year: activeWorkoutState.year,
+                              exercises: activeWorkoutState.exercises);
+                        default:
+                          return Container();
+                      }
                     },
                   ),
                 ),
