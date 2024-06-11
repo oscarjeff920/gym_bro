f2500e7b556feb84cb904c39b97dc1b1c22f46d3
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-06-11 11:56:15 +0100

    Added state logic to determine which previous set to show

--- lib/state_management/blocs/database_tables/exercise_set/get_latest_exercise_sets_by_movement_bloc/get_last_exercise_sets_by_movement_state.dart
+++ lib/state_management/blocs/database_tables/exercise_set/get_latest_exercise_sets_by_movement_bloc/get_last_exercise_sets_by_movement_state.dart
@@ -14,7 +14,38 @@ class GetLastExerciseSetQueryingState
 
 class SuccessfulGetLastExerciseSetsByMovementQueryState
     extends GetLastExerciseSetsByMovementState {
-  final List<Map> lastExerciseSets;
+  final List<Sets> lastExerciseSets;
+
+  provideMatchingPreviousSet(
+      CurrentSet currentSet, List<Sets> completedSets) {
+    int totalCompletedSets = completedSets.length;
+
+    List<Sets> lastTimeWorkingSets = [];
+
+    completedSets.asMap().forEach((index, value) {
+      if (!value.isWarmUp) lastTimeWorkingSets.add(value);
+    });
+
+    int totalSetsLastTime = lastExerciseSets.length;
+    int totalWorkingSetsLastTime = lastTimeWorkingSets.length;
+    int totalWarmupSetsLastTime = totalSetsLastTime - totalWorkingSetsLastTime;
+    Sets comparisonSet = lastExerciseSets[totalCompletedSets];
+
+    // if the user does more sets than last time, no comparison can be made
+    // this is also the case if user wants to do more warm up sets than last time
+    if (
+    totalCompletedSets >= totalSetsLastTime ||
+        currentSet.isWarmUp == true && !comparisonSet.isWarmUp
+    ) {
+      return const CurrentSet();
+    }
+
+    // if (comparisonSet.isWarmUp && currentSet.isWarmUp == false) {
+    //   lastTimeWorkingSets
+    // }
+    // if (comparisonSet.isWarmUp == currentSet.isWarmUp)
+    return comparisonSet;
+  }
 
   SuccessfulGetLastExerciseSetsByMovementQueryState(
       {required this.lastExerciseSets});
