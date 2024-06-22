6aaa6111d981237e16c9f4b4130b6155f76d0e50
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-06-22 12:47:28 +0100

    Added a factory method to convert WorkoutTable model into LoadedWorkoutModel

--- lib/data_models/FE_data_models/workout_data_models.dart
+++ lib/data_models/FE_data_models/workout_data_models.dart
@@ -1,3 +1,5 @@
+import 'package:gym_bro/data_models/database_data_models/tables/workout/workout_object.dart';
+
 import 'exercise_data_models.dart';
 
 class GeneralWorkoutModel {
@@ -30,6 +32,18 @@ class LoadedWorkoutModel extends GeneralWorkoutModel {
       required super.workoutStartTime,
       required super.workoutDuration,
       required this.exercises});
+
+  factory LoadedWorkoutModel.fromTableModel(WorkoutTable tableObj) {
+    LoadedWorkoutModel generatedModel = LoadedWorkoutModel(
+        id: tableObj.id!,
+        day: tableObj.day,
+        month: tableObj.month,
+        year: tableObj.year,
+        workoutStartTime: tableObj.workoutStartTime,
+        workoutDuration: tableObj.duration ?? "- - - -",
+        exercises: []);
+    return generatedModel;
+  }
 }
 
 class NewWorkoutModel extends GeneralWorkoutModel {
