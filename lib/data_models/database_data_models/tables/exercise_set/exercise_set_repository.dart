c066d28441f2f0c0d8075fff7da361bc2ba4696f
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-09-27 14:35:17 +0100

    Forced more explicit typing

--- lib/data_models/database_data_models/tables/exercise_set/exercise_set_repository.dart
+++ lib/data_models/database_data_models/tables/exercise_set/exercise_set_repository.dart
@@ -32,10 +32,10 @@ class ExerciseSetRepository {
         $workoutTableName.day DESC
       LIMIT 1;
       """;
-      List<Map> latestExercise =
+      List<Map<String, dynamic>> latestExercise =
           await txn.rawQuery(queryMostRecentExerciseByMovementIdString);
       if (latestExercise.isEmpty) {
-        List<Map> returnList = [];
+        List<Map<String, dynamic>> returnList = [];
         return {'data': returnList};
       }
 
@@ -49,7 +49,7 @@ class ExerciseSetRepository {
       ORDER BY $exerciseSetTableName.set_order ASC;
       """;
 
-      List<Map> latestExerciseSetsByMovementId =
+      List<Map<String, dynamic>> latestExerciseSetsByMovementId =
           await txn.rawQuery(queryAllExerciseSetsByExerciseIdString);
 
       return {
