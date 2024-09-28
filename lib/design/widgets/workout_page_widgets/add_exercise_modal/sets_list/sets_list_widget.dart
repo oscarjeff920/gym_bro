eca0c8c097644df217e4627f89e0e10805f47efb
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-09-28 16:37:19 +0100

    re-inserted the sets card for cases where theres no previous data on the movement

--- lib/design/widgets/workout_page_widgets/add_exercise_modal/sets_list/sets_list_widget.dart
+++ lib/design/widgets/workout_page_widgets/add_exercise_modal/sets_list/sets_list_widget.dart
@@ -26,82 +26,81 @@ class SetsListContainer extends StatelessWidget {
     return ListView(
       children: [
         if (currentSet != null)
-            BlocBuilder<GetLastExerciseSetsByMovementBloc,
-                GetLastExerciseSetsByMovementState>(
-              builder: (context, state) {
-                String comparisonExerciseDate;
-                GeneralExerciseSetModel comparisonSet;
-                int? comparisonExerciseWorkingSetNumber;
-                int comparisonExerciseTotalWorkingSets;
+          BlocBuilder<GetLastExerciseSetsByMovementBloc,
+              GetLastExerciseSetsByMovementState>(
+            builder: (context, state) {
+              String comparisonExerciseDate;
+              GeneralExerciseSetModel comparisonSet;
+              int? comparisonExerciseWorkingSetNumber;
+              int comparisonExerciseTotalWorkingSets;
 
-                if (state
-                        is SuccessfulGetLastExerciseSetsByMovementQueryState &&
-                    state.lastExerciseSetsData['data'].isNotEmpty) {
-                  List<GeneralExerciseSetModel> lastExerciseSets =
-                      state.lastExerciseSetsData['data'];
-                  GeneralExerciseSetModel movementPR =
-                      state.movementPRData['data'];
+              if (state is SuccessfulGetLastExerciseSetsByMovementQueryState &&
+                  state.lastExerciseSetsData['data'].isNotEmpty) {
+                List<GeneralExerciseSetModel> lastExerciseSets =
+                    state.lastExerciseSetsData['data'];
+                GeneralExerciseSetModel movementPR =
+                    state.movementPRData['data'];
 
-                  // Here we're getting the most relevant set from the previous exercise
-                  // to display and compare to the current
-                  int comparisonExerciseSetIndex =
-                      state.provideMatchingPreviousSetIndex(
-                          currentSet: currentSet!,
-                          completedSets: completedSets,
-                          comparisonExerciseSets: lastExerciseSets);
+                // Here we're getting the most relevant set from the previous exercise
+                // to display and compare to the current
+                int comparisonExerciseSetIndex =
+                    state.provideMatchingPreviousSetIndex(
+                        currentSet: currentSet!,
+                        completedSets: completedSets,
+                        comparisonExerciseSets: lastExerciseSets);
 
-                  return BlocBuilder<DisplayPrCubit, DisplayPrState>(
-                    builder: (prContext, prState_) {
-                      if (prState_.displayedPR) {
-                        comparisonExerciseDate =
-                            state.movementPRData['dateString'];
-                        comparisonSet = movementPR;
-                        comparisonExerciseWorkingSetNumber = null;
-                        // TODO: not a fan of setting to 0, but in the future I can add the real value
-                        comparisonExerciseTotalWorkingSets = 0;
-                      } else {
-                        comparisonExerciseDate =
-                            state.lastExerciseSetsData['dateString'];
-                        comparisonSet =
-                            lastExerciseSets[comparisonExerciseSetIndex];
-                        // If the comparison set is a warm up, working set number = null
-                        comparisonExerciseWorkingSetNumber = comparisonSet
-                                .isWarmUp
-                            ? null
-                            // we want to display the number of working sets up to
-                            // the current working set number, to correctly show
-                            // which working set is being displayed
-                            : state
-                                .getPreviousWorkingSets(lastExerciseSets
-                                    .sublist(0, comparisonExerciseSetIndex + 1))
-                                .length;
+                return BlocBuilder<DisplayPrCubit, DisplayPrState>(
+                  builder: (prContext, prState_) {
+                    if (prState_.displayedPR) {
+                      comparisonExerciseDate =
+                          state.movementPRData['dateString'];
+                      comparisonSet = movementPR;
+                      comparisonExerciseWorkingSetNumber = null;
+                      // TODO: not a fan of setting to 0, but in the future I can add the real value
+                      comparisonExerciseTotalWorkingSets = 0;
+                    } else {
+                      comparisonExerciseDate =
+                          state.lastExerciseSetsData['dateString'];
+                      comparisonSet =
+                          lastExerciseSets[comparisonExerciseSetIndex];
+                      // If the comparison set is a warm up, working set number = null
+                      comparisonExerciseWorkingSetNumber = comparisonSet
+                              .isWarmUp
+                          ? null
+                          // we want to display the number of working sets up to
+                          // the current working set number, to correctly show
+                          // which working set is being displayed
+                          : state
+                              .getPreviousWorkingSets(lastExerciseSets.sublist(
+                                  0, comparisonExerciseSetIndex + 1))
+                              .length;
 
-                        comparisonExerciseTotalWorkingSets = state
-                            .getPreviousWorkingSets(lastExerciseSets)
-                            .length;
-                      }
+                      comparisonExerciseTotalWorkingSets =
+                          state.getPreviousWorkingSets(lastExerciseSets).length;
+                    }
 
-                      return Column(
-                        children: [
-                          PreviousSetCardHeaders(
-                              isPr: prState_.displayedPR,
-                              date: comparisonExerciseDate,
-                              workingSetsCount:
-                                  comparisonExerciseTotalWorkingSets),
-                          PreviousSetCard(
-                              set: comparisonSet,
-                              setNumber: comparisonExerciseWorkingSetNumber),
-                          CurrentSetCard(
-                              currentSet: currentSet,
-                              comparisonSet: comparisonSet)
-                        ],
-                      );
-                    },
-                  );
-                }
-                return Container();//CurrentSetCard(currentSet: currentSet);
-              },
-            ),
+                    return Column(
+                      children: [
+                        PreviousSetCardHeaders(
+                            isPr: prState_.displayedPR,
+                            date: comparisonExerciseDate,
+                            workingSetsCount:
+                                comparisonExerciseTotalWorkingSets),
+                        PreviousSetCard(
+                            set: comparisonSet,
+                            setNumber: comparisonExerciseWorkingSetNumber),
+                        CurrentSetCard(
+                            currentSet: currentSet,
+                            comparisonSet: comparisonSet)
+                      ],
+                    );
+                  },
+                );
+              }
+              // If theres no previous exercise data for this movement:
+              return CurrentSetCard(currentSet: currentSet);
+            },
+          ),
         for (var set in completedSets) CompletedSetCard(set: set)
       ],
     );
