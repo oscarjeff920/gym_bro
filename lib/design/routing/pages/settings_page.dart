65d2e04d52766a92f2fba9fd72749a75aeaa5df9
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-11-12 22:45:19 +0000

    added settings page to place the export db button

---
+++ lib/design/routing/pages/settings_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gym_bro/design/widgets/the_app_bar_widget.dart';
+import 'package:gym_bro/state_management/blocs/database_tables/workout/workout_table_operations_bloc.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: const TheAppBar(
+        hasBackButton: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(
+            flex: 2,
+          ),
+          Expanded(
+              child: TextButton(
+                  style:
+                      const ButtonStyle(elevation: WidgetStatePropertyAll(10)),
+                  onPressed: () {
+                    BlocProvider.of<WorkoutTableOperationsBloc>(context)
+                        .workoutRepository
+                        .exportDatabaseToDownloads();
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Export Database To Downloads"),
+                      Icon(Icons.backup_outlined)
+                    ],
+                  ))),
+          const Spacer(
+            flex: 12,
+          )
+        ],
+      ),
+    );
+  }
+}
