e9e3797fa3d6af773499e9e19ed7fa9f40b7733d
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-12-05 12:43:22 +0000

    Wrapped all important information about muscleGroupTypes into one class, this way the important info, such as icon and colour, can be accessed on the muscle group object, instead of having to run separated functions to obtain the colour and then the icon

--- lib/constants/enums.dart
+++ lib/constants/enums.dart
@@ -5,6 +5,52 @@ enum RoleType {
   secondary,
 }
 
+class MuscleGroup {
+  final MuscleGroupType type;
+  final IconData icon;
+  final Color color;
+
+  const MuscleGroup({
+    required this.type,
+    required this.icon,
+    required this.color,
+  });
+
+  // Static list to hold all muscle groups for easy iteration
+  static final List<MuscleGroup> allMuscleGroups = [
+    const MuscleGroup(
+      type: MuscleGroupType.chest,
+      icon: Icons.favorite_outline_sharp,
+      color: Color.fromRGBO(255, 80, 80, 1),
+    ),
+    const MuscleGroup(
+      type: MuscleGroupType.shoulders,
+      icon: Icons.emoji_people,
+      color: Color.fromRGBO(10, 200, 255, 1),
+    ),
+    const MuscleGroup(
+      type: MuscleGroupType.biceps,
+      icon: Icons.fitness_center,
+      color: Color.fromRGBO(255, 140, 80, 1),
+    ),
+    const MuscleGroup(
+      type: MuscleGroupType.triceps,
+      icon: Icons.expand,
+      color: Color.fromRGBO(240, 220, 100, 1),
+    ),
+    const MuscleGroup(
+      type: MuscleGroupType.back,
+      icon: Icons.rowing,
+      color: Color.fromRGBO(150, 150, 255, 1),
+    ),
+    const MuscleGroup(
+      type: MuscleGroupType.legs,
+      icon: Icons.sports_martial_arts,
+      color: Color.fromRGBO(30, 225, 75, 1),
+    ),
+  ];
+}
+
 enum MuscleGroupType {
   chest,
   shoulders,
@@ -14,30 +60,3 @@ enum MuscleGroupType {
   legs,
 }
 
-IconData assignIcon(MuscleGroupType muscleGroup) {
-  switch (muscleGroup) {
-    case MuscleGroupType.chest:
-      return Icons.favorite_outline_sharp;
-    case MuscleGroupType.shoulders:
-      return Icons.emoji_people;
-    case MuscleGroupType.biceps:
-      return Icons.fitness_center;
-    case MuscleGroupType.triceps:
-      return Icons.expand;
-    case MuscleGroupType.back:
-      return Icons.rowing;
-    case MuscleGroupType.legs:
-      return Icons.sports_martial_arts;
-    default:
-      return Icons.question_mark;
-  }
-}
-
-Map<MuscleGroupType, Color> muscleGroupColours = {
-  MuscleGroupType.chest: const Color.fromRGBO(255, 80, 80, 1),
-  MuscleGroupType.biceps: const Color.fromRGBO(255, 140, 80, 1),
-  MuscleGroupType.triceps: const Color.fromRGBO(240, 220, 100, 1),
-  MuscleGroupType.legs: const Color.fromRGBO(30, 225, 75, 1),
-  MuscleGroupType.shoulders: const Color.fromRGBO(10, 200, 255, 1),
-  MuscleGroupType.back: const Color.fromRGBO(150, 150, 255, 1),
-};
