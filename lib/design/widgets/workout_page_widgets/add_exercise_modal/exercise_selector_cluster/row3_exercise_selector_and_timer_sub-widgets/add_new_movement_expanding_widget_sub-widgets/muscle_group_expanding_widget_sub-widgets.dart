5697a6e2f073b566a1a9e3ff7dd312730c30577a
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-12-05 14:11:37 +0000

    made PrimaryMuscleGroup expanding widget's sub widgets usable for both primary and secondary muscle groups. Added custom raised button to select the relevant muscle groups for the new movement

---
+++ lib/design/widgets/workout_page_widgets/add_exercise_modal/exercise_selector_cluster/row3_exercise_selector_and_timer_sub-widgets/add_new_movement_expanding_widget_sub-widgets/muscle_group_expanding_widget_sub-widgets.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gym_bro/constants/enums.dart';
+import 'package:gym_bro/state_management/cubits/add_exercise_cubit/add_exercise_cubit.dart';
+import 'package:gym_bro/state_management/cubits/add_exercise_cubit/add_exercise_state.dart';
+
+class MuscleGroupHeaderWidget extends StatelessWidget {
+  final bool isPrimary;
+
+  const MuscleGroupHeaderWidget({super.key, required this.isPrimary});
+
+  get muscleRole => isPrimary ? 'Primary' : 'Secondary';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 4.0),
+      child: Text(
+        "$muscleRole Muscle Group(s):",
+        style: const TextStyle(
+          fontSize: 10,
+          color: Color.fromRGBO(10, 10, 10, 0.6),
+        ),
+      ),
+    );
+  }
+}
+
+class MuscleGroupIndicatorRowWidget extends StatelessWidget {
+  final bool isPrimary;
+
+  const MuscleGroupIndicatorRowWidget({super.key, required this.isPrimary});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddExerciseCubit, AddExerciseState>(
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (var muscleGroup in MuscleGroup.allMuscleGroups.values)
+              Expanded(
+                child: RaisedMuscleGroupButton(
+                  muscleGroup: muscleGroup,
+                  isPrimary: isPrimary,
+                ),
+              ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class RaisedMuscleGroupButton extends StatefulWidget {
+  final MuscleGroup muscleGroup;
+  final bool isPrimary;
+
+  const RaisedMuscleGroupButton(
+      {super.key, required this.muscleGroup, required this.isPrimary});
+
+  @override
+  State<RaisedMuscleGroupButton> createState() =>
+      _RaisedMuscleGroupButtonState();
+}
+
+class _RaisedMuscleGroupButtonState extends State<RaisedMuscleGroupButton> {
+  bool _isToggled = false;
+
+  void _toggleButton() {
+    setState(() {
+      _isToggled = !_isToggled;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: ElevatedButton(
+          onPressed: () {
+            _toggleButton();
+          },
+          style: ButtonStyle(
+            padding: const WidgetStatePropertyAll(EdgeInsets.all(0)),
+            elevation: _isToggled
+                ? const WidgetStatePropertyAll(0)
+                : const WidgetStatePropertyAll(3),
+            backgroundColor: _isToggled
+                ? WidgetStatePropertyAll(widget.muscleGroup.colour)
+                : const WidgetStatePropertyAll(Colors.white),
+          ),
+          child: Icon(
+            widget.muscleGroup.icon,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
