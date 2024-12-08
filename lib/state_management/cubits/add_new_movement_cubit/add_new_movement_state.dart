b58d02c1a48c0dd28fc8861196e9380a51aa1cbb
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-12-08 21:15:36 +0000

    added workedMuscleGroups param to AddNewMovementState. Added new method to AddNewMovementCubit to allow toggling of worked muscle groups for the new movement via new muscle group buttons on the expandable widget, along with unit tests for said new method

--- lib/state_management/cubits/add_new_movement_cubit/add_new_movement_state.dart
+++ lib/state_management/cubits/add_new_movement_cubit/add_new_movement_state.dart
@@ -1,16 +1,34 @@
 import 'package:equatable/equatable.dart';
+import 'package:gym_bro/data_models/database_data_models/joined_tables/movement-muscle_group/movement-muscle_group_methods.dart';
 
 class AddNewMovementState extends Equatable {
   final bool isNewMovementSelected;
   final bool showAnimatedChildren;
   final String? movementName;
+  final MovementWorkedMuscleGroupsType? workedMuscleGroups;
 
   const AddNewMovementState(
       {this.isNewMovementSelected = false,
       this.showAnimatedChildren = false,
-      this.movementName});
+      this.movementName,
+      this.workedMuscleGroups});
+
+  AddNewMovementState copyWith(
+      {String? copyMovementName,
+      MovementWorkedMuscleGroupsType? copyWorkedMuscleGroups}) {
+    return AddNewMovementState(
+      isNewMovementSelected: isNewMovementSelected,
+      showAnimatedChildren: showAnimatedChildren,
+      movementName: copyMovementName ?? movementName,
+      workedMuscleGroups: copyWorkedMuscleGroups ?? workedMuscleGroups,
+    );
+  }
 
   @override
-  List<Object?> get props =>
-      [isNewMovementSelected, showAnimatedChildren, movementName];
+  List<Object?> get props => [
+        isNewMovementSelected,
+        showAnimatedChildren,
+        movementName,
+        workedMuscleGroups
+      ];
 }
