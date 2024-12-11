fd218d90f38aaef554cf9d112c7d0d26f29d4905
Oscar Jeffery <oscar.jeffery@15gifts.com>
2024-12-11 13:51:15 +0000

    for the working set indicator, it wasn't clear if the working set was the current set, or the ones completed. I've changed that for the current set so that it displays Current Working Set: (number) instead of just Working Set

--- lib/design/widgets/workout_page_widgets/add_exercise_modal/sets_list/sets_cards/working_warmup_set_header_counter_widget.dart
+++ lib/design/widgets/workout_page_widgets/add_exercise_modal/sets_list/sets_cards/working_warmup_set_header_counter_widget.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class WorkingSetCount extends StatelessWidget {
-  const WorkingSetCount({
-    super.key,
-    required this.setNumber,
-  });
+  const WorkingSetCount(
+      {super.key, required this.setNumber, required this.isCurrent});
 
   final int? setNumber;
+  final bool isCurrent;
+
+  String get workingSetString => isCurrent ? "Current " : "";
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Text(
-          setNumber == null ? "Warm up Set" : "Working Set: ",
+          setNumber == null
+              ? "Warm up Set"
+              : "${workingSetString}Working Set: ",
           style: const TextStyle(
             color: Color.fromRGBO(255, 255, 255, 0.6),
             fontSize: 11,
